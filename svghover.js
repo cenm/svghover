8d16814f4c2c3c93e41d2f25b82966bd24dff8a4
cenm <cenm.lin@gmail.com>
2017-08-09 19:11:49 +0800

    Update svghover.js

--- svghover.js
+++ svghover.js
@@ -2,13 +2,13 @@ function svgClick(url){
 	window.open(url,'_blank');
 }
 
-function svgMouseOver(this){
+function svgMouseOver(){
 	var all = this.getElementsByTagName("path");
 	for (var i=0, max=all.length; i < max; i++) {
 		all[i].style.fill='RED';
 	}
 }
-function svgMouseOut(this){
+function svgMouseOut(){
 	var all = this.getElementsByTagName("path");
 	for (var i=0, max=all.length; i < max; i++) {
 		all[i].style.fill='black';
