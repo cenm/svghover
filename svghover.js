084ebe506a1e0879922ba8a88ea98bf89c773456
cenm <cenm.lin@gmail.com>
2017-08-09 18:56:28 +0800

    Update svghover.js

--- svghover.js
+++ svghover.js
@@ -1,1 +1,17 @@
+function svgClick(url){
+	window.open(url,'_blank');
+}
+
+function svgMouseOver(){
+	var all = this.getElementsByTagName("path");
+	for (var i=0, max=all.length; i < max; i++) {
+		all[i].style.fill='RED'}
+	}
+}
+function svgMouseOut(){
+	var all = this.getElementsByTagName("path");
+	for (var i=0, max=all.length; i < max; i++) {
+		all[i].style.fill='black'}
+	}
+}
 
